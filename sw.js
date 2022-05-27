2e0ce723637ac20c02e78c24201ca2acbe907af9
Solaiman Islam <71056248+Solaiman514@users.noreply.github.com>
2022-05-27 19:57:42 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,54 @@
+const cacheName = 'v3';
+const staticAssets = [
+  'favicon.ico',
+  'hsplayer.js',
+  'index.html',
+  'index.js',
+  'logo192.png',
+  'logo512.png',
+  'manifest.webmanifest',
+  'reset.css',
+  'sw.js',
+  'w3css4.css',
+  'web.config',
+  'windowsetup.js'
+];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
