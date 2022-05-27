b074a3c0dab3a55c5838a3111f232d6dccf225bd
Solaiman Islam <71056248+Solaiman514@users.noreply.github.com>
2022-05-27 19:59:10 +0600

    Create windowsetup.js

---
+++ windowsetup.js
@@ -0,0 +1,13 @@
+window.onresize = function () {
+  windowSize();
+}
+
+document.onreadystatechange = function () {
+  windowSize();
+}
+
+async function windowSize() {
+  document.getElementById('video').style.height = (document.documentElement.clientHeight - 5) + 'px';
+  document.getElementById('video').style.width = (document.documentElement.clientWidth - 5) + 'px';
+  document.getElementById('video').style.overflow = scroll;
+}
